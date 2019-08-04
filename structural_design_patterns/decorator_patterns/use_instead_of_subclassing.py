d64018a1454bc3a9068ffb5a0bbf5a7b6f10a514
emu_yuu <mhk2.747.vclv@gmail.com>
2019-08-04 14:31:32 +0900

    use decorator instead of subclass

--- structural_design_patterns/decorator_patterns/use_instead_of_subclassing.py
+++ structural_design_patterns/decorator_patterns/use_instead_of_subclassing.py
@@ -1,1 +1,33 @@
 # using a class decorator instead of subclassing
+
+# subclass:
+# あるクラスを継承しているクラス。子クラス。
+
+
+def mediated(cls):
+    setattr(cls, 'mediator', None)
+
+    def on_change(self):
+        if self.mediator is not None:
+            self.mediator.on_change(self)
+
+    setattr(cls, 'on_change', on_change)
+    return cls
+
+
+@mediated
+class Mediated:
+
+    def __init__(self):
+        self.mediator = None
+
+    def on_change(self):
+        if self.mediator is not None:
+            self.mediator.on_change(self)
+
+    def __repr__(self):
+        return str(self.mediator)
+
+
+test = Mediated()
+print(test)
