c77c98e42941d13d04e55505145f27f7fbd1229d
HarukaMaekawa <maekawa@qbit-robotics.jp>
2019-09-13 11:05:53 +0900

    observer pattern

---
+++ 3_behavioral_design_patterns/observer_pattern.py
@@ -0,0 +1,70 @@
+# observer pattern
+# > "監視対象(Subject)が変化した時に監視者(Observer)に通知する仕組み"
+
+# 参考・引用：https://qiita.com/Algos/items/ba9c6ed52380b57aecac
+
+# ---------------------------------------
+
+
+class Subject:
+    def __init__(self):
+        self._observers = []
+
+    def add_observer(self, observer):
+        if observer not in self._observers:
+            self._observers.append(observer)
+
+    def detach_observer(self, observer):
+        try:
+            self._observers.remove(observer)
+        except ValueError:
+            pass
+
+    def notify(self):
+        for observer in self._observers:
+            observer.update()
+
+
+class Button(Subject):
+    def __init__(self):
+        Subject.__init__(self)
+        self._button_state = 0
+
+    @property
+    def button_state(self):
+        return self._button_state
+
+    @button_state.setter
+    def button_state(self, state):
+        if self._button_state != state:
+            self._button_state = state
+            self.notify()
+
+
+class Observer1:
+    @staticmethod
+    def update():
+        print('Observer1 notified! Button changed')
+
+
+class Observer2:
+    @staticmethod
+    def update():
+        print('Observer2 notified! Button changed')
+
+
+if __name__ == '__main__':
+    observer1 = Observer1()
+    observer2 = Observer2()
+
+    button = Button()
+    button.add_observer(observer1)
+    button.add_observer(observer2)
+
+    button.button_state = 10
+
+    print('---------------------------')
+
+    button.detach_observer(observer1)
+
+    button.button_state = 20
